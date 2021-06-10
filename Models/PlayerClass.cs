f029fbd5e5934ed567bb7ce5793daf81ff1700c1
agarwal30shubham@gmail.com <agarwal30shubham@gmail.com>
2021-06-10 02:37:32 -0700

    Second Commit

---
+++ Models/PlayerClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreMvcGitExercise.Models
+{
+    [Table("Players")]
+    public class PlayerClass
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Team { get; set; }
+    }
+}
